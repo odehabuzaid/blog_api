be0f016927bb9ad5012e89cd260f616cedc62126
Odeh <abuzaid163@hotmail.com>
2021-11-22 00:12:24 +0200

    django-REST api completed added CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Post
+from .serializer import PostSerializer
+
+
+class PostsList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
--- blog_api/urls.py
+++ blog_api/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
